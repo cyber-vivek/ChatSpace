87d0a47030c0f0e70875b686abad1d29ef2b8e5f
Vivek Kumar Verma <vivekkr3580@gmail.com>
2024-02-18 00:55:04 +0530

    Route setup in frontend and db, serve setup in backend

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Chat from './pages/Chat';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Chat/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('db connection successfull')
+}).catch( (err) => {
+    console.log('could not connect to db')
+})
+
+const server = app.listen(process.env.PORT, () => {
+    console.log('Server Started Successfully');
+})
